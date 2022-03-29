b2188debe32140d660098f93391f966cc8acb774
Sugumar Prabhakaran <sug.prabhakaran@gmail.com>
2022-03-28 20:36:11 -0400

    replaced deprecated PyKDL tf_conversions module with transforms3d

--- lab3_lidar_mapping/nodes/utils.py
+++ lab3_lidar_mapping/nodes/utils.py
@@ -1,7 +1,8 @@
 # Various convenience functions for conversions, etc.
 
 import numpy as np
-import tf_conversions
+from transforms3d import euler, quaternions
+#import tf_conversions
 from geometry_msgs.msg import Transform, Pose, Quaternion
 
 
@@ -25,13 +26,16 @@ def convert_tf_to_pose(tf):
 
 def euler_from_ros_quat(q):
     # get the SXYZ euler angles from a ros XYZW quaternion
-    np_q = np.array([q.x, q.y, q.z, q.w])
-    return tf_conversions.transformations.euler_from_quaternion(np_q)
+    np_q = np.array([q.w, q.x, q.y, q.z])
+    #return tf_conversions.transformations.euler_from_quaternion(np_q)
+    eulers = euler.quat2euler(np_q, axes='sxyz')
+    return eulers
 
 def ros_quat_from_euler(e):
     # get a ROS XYZW quaternion from an SXYZ euler
-    np_q = tf_conversions.transformations.quaternion_from_euler(*e)
-    return ros_q_from_np_q(np_q)
+    #np_q = tf_conversions.transformations.quaternion_from_euler(*e)
+    np_q = euler.euler2quat(*e)     # form [w, x, y, z] need in form [x, y, z, w]
+    return ros_q_from_np_q(np_q) 
 
 def np_q_from_ros_q(q):
     q = np.array([q.x, q.y, q.z, q.w])
@@ -39,18 +43,22 @@ def np_q_from_ros_q(q):
 
 def ros_q_from_np_q(np_q):
     q = Quaternion()
-    q.x = np_q[0]; q.y = np_q[1]; q.z = np_q[2]; q.w = np_q[3]
+    q.x = np_q[1]; q.y = np_q[2]; q.z = np_q[3]; q.w = np_q[0]      # edited q.w = q[0]
     return q
 
 def tf_to_tf_mat(tf):
     # convert ros transform to 4x4 np se3 matrix
-    mat = tf_conversions.transformations.quaternion_matrix(np_q_from_ros_q(tf.rotation))
-    mat[:3, 3] = [tf.translation.x, tf.translation.y, tf.translation.z]
+    #mat = tf_conversions.transformations.quaternion_matrix(np_q_from_ros_q(tf.rotation))
+    mat = quaternions.quat2mat(np_q_from_ros_q(tf.rotation))
+    mat = np.hstack((mat, [[tf.translation.x], [tf.translation.y], [tf.translation.z]]))
+    print("final tf_mat:\n", mat)
+
     return mat
 
 def tf_mat_to_tf(tf_mat):
     # convert 4x4 np se3 matrix to ros transform
     tf = Transform()
     [tf.translation.x, tf.translation.y, tf.translation.z] = tf_mat[:3, 3]
-    tf.rotation = ros_q_from_np_q(tf_conversions.transformations.quaternion_from_matrix(tf_mat))
+    #tf.rotation = ros_q_from_np_q(tf_conversions.transformations.quaternion_from_matrix(tf_mat))
+    tf.rotation = ros_q_from_np_q(quaternions.mat2quat(tf_mat[:3,:3]))
     return tf
